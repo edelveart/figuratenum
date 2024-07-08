dd46f71471355c3d7596c368b979567a0bbe637f
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-08 00:02:19 -0500

    feat: add cubic numbers

--- figurate_numbers/space_figurate_numbers.py
+++ figurate_numbers/space_figurate_numbers.py
@@ -6,3 +6,10 @@ def m_pyramidal_numbers(m) -> Iterator[int]:
     while True:
         yield (delta * (delta + 1) * ((m - 2) * delta - m + 5)) // 6
         delta += 1
+
+
+def cubic_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield delta ** 3
+        delta += 1
