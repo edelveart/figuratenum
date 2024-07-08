318dd49ff5ebe11156df8eea873caff536d1b612
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-07 22:48:39 -0500

    feat: add polygonal and centered mgonal numbers

---
+++ figurate_numbers/plane_figurate_numbers.py
@@ -0,0 +1,16 @@
+from math import log2, floor
+from typing import Iterator
+
+
+def polygonal_numbers(m: int) -> Iterator[int]:
+    delta = 1
+    while True:
+        yield ((m - 2) * delta ** 2 - (m - 4) * delta) // 2
+        delta += 1
+
+
+def centered_mgonal_numbers(m: int) -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (m * delta ** 2 - m * delta + 2) // 2
+        delta += 1
