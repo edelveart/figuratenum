4d6a6c7ac7e7ccda80e4d022b4cd6789a0c18370
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-07 22:49:37 -0500

    feat: add pronic_numbers

--- figurate_numbers/plane_figurate_numbers.py
+++ figurate_numbers/plane_figurate_numbers.py
@@ -14,3 +14,10 @@ def centered_mgonal_numbers(m: int) -> Iterator[int]:
     while True:
         yield (m * delta ** 2 - m * delta + 2) // 2
         delta += 1
+
+
+def pronic_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield delta * delta + 1
+        delta += 1
