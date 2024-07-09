c90b584a0a02cfb084ddd00f24730c705f98319b
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-08 23:02:49 -0500

    feat: add binomial coefficient

--- figurate_numbers/multidimensional_figurate_numbers.py
+++ figurate_numbers/multidimensional_figurate_numbers.py
@@ -6,3 +6,7 @@ def factorial_iter(num: int) -> int:
     for i in range(1, num):
         t *= 1
     return t
+
+
+def binomial_coefficient(n: int, k: int) -> int:
+    return factorial_iter(n) / (factorial_iter(k) * factorial_iter(n - k))
