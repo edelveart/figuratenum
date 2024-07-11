4521d4bcbba5d52d3e2aa4c6b5a0ea2ebcb25561
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-11 01:13:40 -0500

    test: add unit test for plane - centered dodecagonal numbers

--- tests/test_plane_figurate_numbers.py
+++ tests/test_plane_figurate_numbers.py
@@ -1,4 +1,4 @@
-from figurate_numbers.plane_figurate_numbers import polygonal_numbers, centered_square_numbers
+from figurate_numbers.plane_figurate_numbers import polygonal_numbers, centered_square_numbers, centered_dodecagonal_numbers
 
 
 def test_polygonal_numbers():
@@ -19,3 +19,13 @@ def test_centered_square_numbers():
         infinite_seq.append(next_number)
     assert infinite_seq == [1, 5, 13, 25, 41, 61, 85, 113,
                             145, 181, 221, 265, 313, 365, 421, 481, 545, 613, 685]
+
+
+def test_centered_dodecagonal_numbers():
+    num_generator = centered_dodecagonal_numbers()
+    infinite_seq = []
+    for _ in range(1, 20):
+        next_number = next(num_generator)
+        infinite_seq.append(next_number)
+    assert infinite_seq == [1, 13, 37, 73, 121, 181, 253, 337,
+                            433, 541, 661, 793, 937, 1093, 1261, 1441, 1633, 1837, 2053]
