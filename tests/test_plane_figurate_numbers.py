266438152c1d91e4bd049fbfdf30e5cc15de5a68
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-11 01:18:41 -0500

    test: add unit test for plane - polite numbers

--- tests/test_plane_figurate_numbers.py
+++ tests/test_plane_figurate_numbers.py
@@ -1,4 +1,4 @@
-from figurate_numbers.plane_figurate_numbers import polygonal_numbers, centered_square_numbers, centered_dodecagonal_numbers, centered_mgonal_numbers, pronic_numbers
+from figurate_numbers.plane_figurate_numbers import polygonal_numbers, centered_square_numbers, centered_dodecagonal_numbers, centered_mgonal_numbers, pronic_numbers, polite_numbers
 
 
 def test_polygonal_numbers():
@@ -49,3 +49,13 @@ def test_pronic_numbers():
         infinite_seq.append(next_number)
     assert infinite_seq == [2, 5, 10, 17, 26, 37, 50, 65,
                             82, 101, 122, 145, 170, 197, 226, 257, 290, 325, 362]
+
+
+def test_polite_numbers():
+    num_generator = polite_numbers()
+    infinite_seq = []
+    for _ in range(1, 20):
+        next_number = next(num_generator)
+        infinite_seq.append(next_number)
+    assert infinite_seq == [1, 3, 5, 6, 7, 9, 10, 11,
+                            12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23]
