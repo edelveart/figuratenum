e225561fe38e588e06514583398e77d500a44d73
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-11 01:12:19 -0500

    test: add unit test for plane - centered square numbers

--- tests/test_plane_figurate_numbers.py
+++ tests/test_plane_figurate_numbers.py
@@ -1,4 +1,4 @@
-from figurate_numbers.plane_figurate_numbers import polygonal_numbers
+from figurate_numbers.plane_figurate_numbers import polygonal_numbers, centered_square_numbers
 
 
 def test_polygonal_numbers():
@@ -9,3 +9,13 @@ def test_polygonal_numbers():
         infinite_seq.append(next_number)
     assert infinite_seq == [1, 6, 15, 28, 45, 66, 91, 120, 153,
                             190, 231, 276, 325, 378, 435, 496, 561, 630, 703]
+
+
+def test_centered_square_numbers():
+    num_generator = centered_square_numbers()
+    infinite_seq = []
+    for _ in range(1, 20):
+        next_number = next(num_generator)
+        infinite_seq.append(next_number)
+    assert infinite_seq == [1, 5, 13, 25, 41, 61, 85, 113,
+                            145, 181, 221, 265, 313, 365, 421, 481, 545, 613, 685]
