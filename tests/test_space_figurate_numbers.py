4efab7bd7011272a21f125a97e856df36c6ae977
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-12 03:58:30 -0500

    test: add unit test for space - generalized octahedral numbers

--- tests/test_space_figurate_numbers.py
+++ tests/test_space_figurate_numbers.py
@@ -6,7 +6,7 @@ from figurate_numbers.space_figurate_numbers import (
     hauy_rhombic_dodecahedral_numbers, centered_tetrahedron_numbers,
     centered_square_pyramid_numbers, centered_mgonal_pyramid_numbers,
     centered_octahedron_numbers, centered_icosahedron_numbers, centered_dodecahedron_numbers,
-    centered_truncated_tetrahedron_numbers, centered_truncated_cube_numbers, centered_truncated_octahedron_numbers, centered_mgonal_pyramidal_numbers, centered_hexagonal_pyramidal_numbers, hexagonal_prism_numbers, mgonal_prism_numbers, generalized_mgonal_pyramidal_numbers, generalized_cubic_numbers
+    centered_truncated_tetrahedron_numbers, centered_truncated_cube_numbers, centered_truncated_octahedron_numbers, centered_mgonal_pyramidal_numbers, centered_hexagonal_pyramidal_numbers, hexagonal_prism_numbers, mgonal_prism_numbers, generalized_mgonal_pyramidal_numbers, generalized_cubic_numbers, generalized_octahedral_numbers
 )
 
 
@@ -288,3 +288,13 @@ def test_generalized_cubic_numbers():
         infinite_seq.append(next_number)
     assert infinite_seq == [-1000, -729, -512, -343, -216, -
                             125, -64, -27, -8, -1, 0, 1, 8, 27, 64, 125, 216, 343, 512]
+
+
+def test_generalized_octahedral_numbers():
+    num_generator = generalized_octahedral_numbers(-10)
+    infinite_seq = []
+    for _ in range(1, 20):
+        next_number = next(num_generator)
+        infinite_seq.append(next_number)
+    assert infinite_seq == [-670, -489, -344, -231, -146, -
+                            85, -44, -19, -6, -1, 0, 1, 6, 19, 44, 85, 146, 231, 344]
