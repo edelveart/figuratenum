74f5f3e45345f26d147d6322c821c31f92ecd78a
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-12 03:38:21 -0500

    test: add unit test for space - stella octangula numbers

--- tests/test_space_figurate_numbers.py
+++ tests/test_space_figurate_numbers.py
@@ -1,7 +1,8 @@
 from figurate_numbers.space_figurate_numbers import (
     m_pyramidal_numbers, cubic_numbers, tetrahedral_numbers,
     octahedral_numbers, dodecahedral_numbers, icosahedral_numbers,
-    truncated_tetrahedral_numbers, truncated_cubic_numbers, truncated_octahedral_numbers
+    truncated_tetrahedral_numbers, truncated_cubic_numbers, truncated_octahedral_numbers,
+    stella_octangula_numbers
 )
 
 
@@ -93,3 +94,13 @@ def test_truncated_octahedral_numbers():
         infinite_seq.append(next_number)
     assert infinite_seq == [1, 38, 201, 586, 1289, 2406, 4033, 6266, 9201,
                             12934, 17561, 23178, 29881, 37766, 46929, 57466, 69473, 83046, 98281]
+
+
+def test_stella_octangula_numbers():
+    num_generator = stella_octangula_numbers()
+    infinite_seq = []
+    for _ in range(1, 20):
+        next_number = next(num_generator)
+        infinite_seq.append(next_number)
+    assert infinite_seq == [1, 14, 51, 124, 245, 426, 679, 1016, 1449,
+                            1990, 2651, 3444, 4381, 5474, 6735, 8176, 9809, 11646, 13699]
