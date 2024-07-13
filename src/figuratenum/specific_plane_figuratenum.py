497c367d805ecf94fc55fd08abae53e107fc4687
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-13 18:18:56 -0500

    feat: add specific cases of centered mgonal numbers

--- src/figuratenum/specific_plane_figuratenum.py
+++ src/figuratenum/specific_plane_figuratenum.py
@@ -197,3 +197,188 @@ def triacontagonal_numbers() -> Iterator[int]:
     while True:
         yield (28 * delta ** 2 - 26 * delta) // 2
         delta += 1
+
+
+# specific cases of centered_mgonal_numbers(m)
+
+
+def centered_triangular_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (3 * delta ** 2 - 3 * delta + 2) // 2
+        delta += 1
+
+
+def centered_pentagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (5 * delta ** 2 - 5 * delta + 2) // 2
+        delta += 1
+
+
+def centered_hexagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 3 * delta ** 2 - 3 * delta + 1
+        delta += 1
+
+
+def centered_heptagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (7 * delta ** 2 - 7 * delta + 2) // 2
+        delta += 1
+
+
+def centered_octagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 4 * delta ** 2 - 4 * delta + 1
+        delta += 1
+
+
+def centered_nonagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (9 * delta ** 2 - 9 * delta + 2) // 2
+        delta += 1
+
+
+def centered_decagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 5 * delta ** 2 - 5 * delta + 1
+        delta += 1
+
+
+def centered_hendecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (11 * delta ** 2 - 11 * delta + 2) // 2
+        delta += 1
+
+
+def centered_tridecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (13 * delta ** 2 - 13 * delta + 2) // 2
+        delta += 1
+
+
+def centered_tetradecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 7 * delta ** 2 - 7 * delta + 1
+        delta += 1
+
+
+def centered_pentadecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (15 * delta**2 - 15 * delta + 2) // 2
+        delta += 1
+
+
+def centered_hexadecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 8 * delta ** 2 - 8 * delta + 1
+        delta += 1
+
+
+def centered_heptadecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (17 * delta**2 - 17 * delta + 2) // 2
+        delta += 1
+
+
+def centered_octadecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 9 * delta ** 2 - 9 * delta + 1
+        delta += 1
+
+
+def centered_nonadecagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (19 * delta ** 2 - 19 * delta + 2) // 2
+        delta += 1
+
+
+def centered_icosagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 10 * delta ** 2 - 10 * delta + 1
+        delta += 1
+
+
+def centered_icosihenagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (21 * delta ** 2 - 21 * delta + 2) // 2
+        delta += 1
+
+
+def centered_icosidigonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 11 * delta ** 2 - 11 * delta + 1
+        delta += 1
+
+
+def centered_icositrigonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (23 * delta ** 2 - 23 * delta + 2) // 2
+        delta += 1
+
+
+def centered_icositetragonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 12 * delta ** 2 - 12 * delta + 1
+        delta += 1
+
+
+def centered_icosipentagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (25 * delta ** 2 - 25 * delta + 2) // 2
+        delta += 1
+
+
+def centered_icosihexagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 13 * delta ** 2 - 13 * delta + 1
+        delta += 1
+
+
+def centered_icosiheptagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (27 * delta ** 2 - 27 * delta + 2) // 2
+        delta += 1
+
+
+def centered_icosioctagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 14 * delta ** 2 - 14 * delta + 1
+        delta += 1
+
+
+def centered_icosinonagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield (29 * delta ** 2 - 29 * delta + 2) // 2
+        delta += 1
+
+
+def centered_triacontagonal_numbers() -> Iterator[int]:
+    delta = 1
+    while True:
+        yield 15 * delta ** 2 - 15 * delta + 1
+        delta += 1
