493469e2a1a868ab9258018b3c3a69781eb066b3
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-18 03:37:54 -0500

    docs: add docstrings to FigurateNum class

--- src/figuratenum/class_figuratenum.py
+++ src/figuratenum/class_figuratenum.py
@@ -6,5 +6,25 @@ from .class_multidimensional_figuratenum import MultidimensionalFigurateNum
 
 
 class FigurateNum(PlaneFigurateNum, SpaceFigurateNum, MultidimensionalFigurateNum):
+    """
+    Generates plane, space and multidimensional figurate numbers.
+
+    Methods
+    -------
+    take(n: int) -> List[int]
+        Returns the first n figurate numbers as a list.
+
+    take_to_array(n: int) -> array
+        Returns the first n figurate numbers as an array.
+
+    Examples
+    --------
+    >>> from figuratenum import FigurateNumber as fg
+    >>> print(fg.generalized_dodecahedral_numbers(-3).take(8))
+    [-165, -56, -10, 0, 1, 20, 84, 220]
+    >>> print(fg.octadecagonal_pyramidal_numbers().take_to_array(5))
+    array('i', [1, 19, 70, 170, 335])
+    """
+
     def __init__(self, generator: Iterator[int]):
         self.generator = generator
