d0f879cc2966efd1c384ccae1c2323d484dcf2c8
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-17 23:26:04 -0500

    feat: add specific cases to SpaceFigurateNum class

--- src/figuratenum/class_space_figuratenum.py
+++ src/figuratenum/class_space_figuratenum.py
@@ -17,6 +17,30 @@ from .space_figuratenum import (
     generalized_mgonal_prism_numbers, generalized_hexagonal_prism_numbers
 )
 
+from .specific_space_figuratenum import (
+    triangular_pyramidal_numbers, square_pyramidal_numbers, pyramidal_numbers, pentagonal_pyramidal_numbers,
+    hexagonal_pyramidal_numbers, heptagonal_pyramidal_numbers, octagonal_pyramidal_numbers,
+    nonagonal_pyramidal_numbers, decagonal_pyramidal_numbers, hendecagonal_pyramidal_numbers,
+    dodecagonal_pyramidal_numbers, tridecagonal_pyramidal_numbers, tetradecagonal_pyramidal_numbers,
+    pentadecagonal_pyramidal_numbers, hexadecagonal_pyramidal_numbers, heptadecagonal_pyramidal_numbers,
+    octadecagonal_pyramidal_numbers, nonadecagonal_pyramidal_numbers, icosagonal_pyramidal_numbers,
+    icosihenagonal_pyramidal_numbers, icosidigonal_pyramidal_numbers, icositrigonal_pyramidal_numbers,
+    icositetragonal_pyramidal_numbers, icosipentagonal_pyramidal_numbers, icosihexagonal_pyramidal_numbers,
+    icosiheptagonal_pyramidal_numbers, icosioctagonal_pyramidal_numbers,
+    icosinonagonal_pyramidal_numbers, triacontagonal_pyramidal_numbers,
+    triangular_tetrahedral_numbers, triangular_square_pyramidal_numbers, square_tetrahedral_numbers,
+    square_square_pyramidal_numbers, tetrahedral_square_pyramidal_numbers,
+
+
+    centered_pentagonal_pyramid_numbers, centered_hexagonal_pyramid_numbers,
+    centered_heptagonal_pyramid_numbers, centered_octagonal_pyramid_numbers,
+
+    centered_triangular_pyramidal_numbers, centered_square_pyramidal_numbers, centered_pentagonal_pyramidal_numbers,
+    centered_heptagonal_pyramidal_numbers, centered_octagonal_pyramidal_numbers, centered_nonagonal_pyramidal_numbers,
+    centered_decagonal_pyramidal_numbers, centered_hendecagonal_pyramidal_numbers, centered_dodecagonal_pyramidal_numbers
+
+)
+
 
 class SpaceFigurateNum:
     def __init__(self, generator: Iterator[int]):
@@ -195,3 +219,193 @@ class SpaceFigurateNum:
     @classmethod
     def generalized_hexagonal_prism_numbers(cls, start_num: int = 0) -> "SpaceFigurateNum":
         return cls(generalized_hexagonal_prism_numbers(start_num))
+
+    # specific space figuratenum
+
+    @classmethod
+    def triangular_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(triangular_pyramidal_numbers())
+
+    @classmethod
+    def square_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(square_pyramidal_numbers())
+
+    @classmethod
+    def pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(pyramidal_numbers())
+
+    @classmethod
+    def pentagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(pentagonal_pyramidal_numbers())
+
+    @classmethod
+    def hexagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(hexagonal_pyramidal_numbers())
+
+    @classmethod
+    def heptagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(heptagonal_pyramidal_numbers())
+
+    @classmethod
+    def octagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(octagonal_pyramidal_numbers())
+
+    @classmethod
+    def nonagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(nonagonal_pyramidal_numbers())
+
+    @classmethod
+    def decagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(decagonal_pyramidal_numbers())
+
+    @classmethod
+    def hendecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(hendecagonal_pyramidal_numbers())
+
+    @classmethod
+    def dodecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(dodecagonal_pyramidal_numbers())
+
+    @classmethod
+    def tridecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(tridecagonal_pyramidal_numbers())
+
+    @classmethod
+    def tetradecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(tetradecagonal_pyramidal_numbers())
+
+    @classmethod
+    def pentadecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(pentadecagonal_pyramidal_numbers())
+
+    @classmethod
+    def hexadecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(hexadecagonal_pyramidal_numbers())
+
+    @classmethod
+    def heptadecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(heptadecagonal_pyramidal_numbers())
+
+    @classmethod
+    def octadecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(octadecagonal_pyramidal_numbers())
+
+    @classmethod
+    def nonadecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(nonadecagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosihenagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosihenagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosidigonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosidigonal_pyramidal_numbers())
+
+    @classmethod
+    def icositrigonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icositrigonal_pyramidal_numbers())
+
+    @classmethod
+    def icositetragonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icositetragonal_pyramidal_numbers())
+
+    @classmethod
+    def icosipentagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosipentagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosihexagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosihexagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosiheptagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosiheptagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosioctagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosioctagonal_pyramidal_numbers())
+
+    @classmethod
+    def icosinonagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(icosinonagonal_pyramidal_numbers())
+
+    @classmethod
+    def triacontagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(triacontagonal_pyramidal_numbers())
+
+    @classmethod
+    def triangular_tetrahedral_numbers(cls) -> "SpaceFigurateNum":
+        return cls(triangular_tetrahedral_numbers())
+
+    @classmethod
+    def triangular_square_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(triangular_square_pyramidal_numbers())
+
+    @classmethod
+    def square_tetrahedral_numbers(cls) -> "SpaceFigurateNum":
+        return cls(square_tetrahedral_numbers())
+
+    @classmethod
+    def square_square_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(square_square_pyramidal_numbers())
+
+    @classmethod
+    def tetrahedral_square_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(tetrahedral_square_pyramidal_numbers())
+
+    @classmethod
+    def centered_pentagonal_pyramid_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_pentagonal_pyramid_numbers())
+
+    @classmethod
+    def centered_hexagonal_pyramid_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_hexagonal_pyramid_numbers())
+
+    @classmethod
+    def centered_heptagonal_pyramid_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_heptagonal_pyramid_numbers())
+
+    @classmethod
+    def centered_octagonal_pyramid_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_octagonal_pyramid_numbers())
+
+    @classmethod
+    def centered_triangular_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_triangular_pyramidal_numbers())
+
+    @classmethod
+    def centered_square_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_square_pyramidal_numbers())
+
+    @classmethod
+    def centered_pentagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_pentagonal_pyramidal_numbers())
+
+    @classmethod
+    def centered_heptagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_heptagonal_pyramidal_numbers())
+
+    @classmethod
+    def centered_octagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_octagonal_pyramidal_numbers())
+
+    @classmethod
+    def centered_nonagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_nonagonal_pyramidal_numbers())
+
+    @classmethod
+    def centered_decagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_decagonal_pyramidal_numbers())
+
+    @classmethod
+    def centered_hendecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_hendecagonal_pyramidal_numbers())
+
+    @classmethod
+    def centered_dodecagonal_pyramidal_numbers(cls) -> "SpaceFigurateNum":
+        return cls(centered_dodecagonal_pyramidal_numbers())
