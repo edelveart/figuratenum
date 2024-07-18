1f157b04f32a7470d2a8861c487e4aa00108fc08
Edgar Delgado Vega <edelve91@gmail.com>
2024-07-18 01:43:18 -0500

    refactor: remove take methods

--- src/figuratenum/class_figuratenum.py
+++ src/figuratenum/class_figuratenum.py
@@ -1,4 +1,4 @@
-from typing import Iterator, List
+from typing import Iterator
 
 from .class_plane_figuratenum import PlaneFigurateNum
 from .class_space_figuratenum import SpaceFigurateNum
@@ -8,9 +8,3 @@ from .class_multidimensional_figuratenum import MultidimensionalFigurateNum
 class FigurateNum(PlaneFigurateNum, SpaceFigurateNum, MultidimensionalFigurateNum):
     def __init__(self, generator: Iterator[int]):
         self.generator = generator
-
-    def take(self, n: int) -> List[int]:
-        result = []
-        for _ in range(n):
-            result.append(next(self.generator))
-        return result
