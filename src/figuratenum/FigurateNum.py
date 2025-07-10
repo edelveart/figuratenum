ee2dcbdfb3f620ff5755f8faf9e7caebf55b2ff1
Edgar Delgado Vega <edelve91@gmail.com>
2025-07-10 18:37:48 -0500

    feat(figurate-num): add ZooFigurateNum to FigurateNum inheritance hierarchy

--- src/figuratenum/FigurateNum.py
+++ src/figuratenum/FigurateNum.py
@@ -1,7 +1,8 @@
 from .plane_figurate_numbers.PlaneFigurateNum import PlaneFigurateNum
 from .space_figurate_numbers.SpaceFigurateNum import SpaceFigurateNum
 from .multidimensional_figurate_numbers.MultidimensionalFigurateNum import MultidimensionalFigurateNum
+from .zoo_figurate_numbers.ZooFigurateNum import ZooFigurateNum
 
 
-class FigurateNum(PlaneFigurateNum, SpaceFigurateNum, MultidimensionalFigurateNum):
+class FigurateNum(PlaneFigurateNum, SpaceFigurateNum, MultidimensionalFigurateNum, ZooFigurateNum):
     pass
