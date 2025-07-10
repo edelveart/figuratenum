91c12d662a16543d8c873d8442b69d054eee3314
Edgar Delgado Vega <edelve91@gmail.com>
2025-07-10 18:15:55 -0500

    feat(zoo-figuratenum): add pell numbers

--- src/figuratenum/zoo_figurate_numbers/zoo_figuratenum.py
+++ src/figuratenum/zoo_figurate_numbers/zoo_figuratenum.py
@@ -1,8 +1,20 @@
 from typing import Iterator
 
 
-def cuban_numbers() -> Iterator[int]:
+def cuban_prime() -> Iterator[int]:
     delta = 1
     while True:
         yield (delta + 1)**3 - delta**3
         delta += 1
+
+
+def pell():
+    pell_numbers = [0, 1]
+    yield 0
+    yield 1
+    i = 2
+    while True:
+        next_val = 2 * pell_numbers[i - 1] + pell_numbers[i - 2]
+        pell_numbers.append(next_val)
+        yield next_val
+        i += 1
