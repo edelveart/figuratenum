08e7f0b9e3384cf0a6326f6300d4ce7831b69d38
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-04 00:33:59 -0500

    fix(zoo-figuratenum): add missing import in __init__.py

--- src/figuratenum/__init__.py
+++ src/figuratenum/__init__.py
@@ -2,4 +2,5 @@ from .FigurateNum import FigurateNum
 from .plane_figurate_numbers.PlaneFigurateNum import PlaneFigurateNum
 from .space_figurate_numbers.SpaceFigurateNum import SpaceFigurateNum
 from .multidimensional_figurate_numbers.MultidimensionalFigurateNum import MultidimensionalFigurateNum
+from .zoo_figurate_numbers.ZooFigurateNum import ZooFigurateNum
 from .NumCollector import NumCollector
