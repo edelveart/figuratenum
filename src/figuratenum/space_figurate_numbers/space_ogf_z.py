c5e5893b96ec6466822ced9836115babed850132
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-05 13:08:48 -0500

    feat(space-ogf-z): add tetrahedral generating function f(z)

--- src/figuratenum/space_figurate_numbers/space_ogf_z.py
+++ src/figuratenum/space_figurate_numbers/space_ogf_z.py
@@ -5,6 +5,10 @@ def m_pyramidal(z: np.ndarray, m: int) -> np.ndarray:
     return z * (1 + (m - 3)*z) / (1 - z)**4
 
 
+def tetrahedral(z: np.ndarray) -> np.ndarray:
+    return z / (1 - z)**4
+
+
 def cubic(z: np.ndarray) -> np.ndarray:
     return z * (1 + 4*z + z**2) / (1 - z)**4
 
@@ -13,5 +17,5 @@ def octahedral(z: np.ndarray) -> np.ndarray:
     return z * (z + 1)**2 / (1 - z)**4
 
 
-def hauy_octahedral(z) -> np.ndarray:
+def hauy_octahedral(z: np.ndarray) -> np.ndarray:
     return z * (1 + z)**3 / (1 - z)**4
