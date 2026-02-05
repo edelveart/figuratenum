fb2b539e935b01eb21c6591f4bceb4e231e84f40
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-04 23:26:28 -0500

    feat(plane-generating-fz): add square generating function f(z)

--- src/figuratenum/plane_figurate_numbers/plane_generating_fz.py
+++ src/figuratenum/plane_figurate_numbers/plane_generating_fz.py
@@ -9,6 +9,10 @@ def triangular(z: np.ndarray) -> np.ndarray:
     return z / (1 - z)**3
 
 
+def square(z: np.ndarray) -> np.ndarray:
+    return z * (z + 1) / (1 - z)**3
+
+
 def centered_m_gonal(z: np.ndarray, m: int = 3) -> np.ndarray:
     return z * ((1 + (m - 2)*z + z**2)) / (1 - z)**3
 
