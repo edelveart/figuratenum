db0c1cc8e735534553d53b814c3eb19fa3c14a5d
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-05 12:49:37 -0500

    feat(space-ogf-z): add cubic generating function f(z)

--- src/figuratenum/space_figurate_numbers/space_ogf_z.py
+++ src/figuratenum/space_figurate_numbers/space_ogf_z.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def m_pyramidal(z: np.ndarray, m: int) -> np.ndarray:
     return z * (1 + (m - 3)*z) / (1 - z)**4
+
+
+def cubic(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 4*z + z**2) / (1 - z)**4
