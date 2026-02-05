3dbc7a0176cf5c5693bd48368b39254a1aa82186
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-04 23:25:48 -0500

    feat(plane-generating-fz): add triangular generating function f(z)

--- src/figuratenum/plane_figurate_numbers/plane_generating_fz.py
+++ src/figuratenum/plane_figurate_numbers/plane_generating_fz.py
@@ -5,6 +5,10 @@ def m_gonal(z: np.ndarray, m: int = 3) -> np.ndarray:
     return z * ((m - 3)*z + 1) / (1 - z)**3
 
 
+def triangular(z: np.ndarray) -> np.ndarray:
+    return z / (1 - z)**3
+
+
 def centered_m_gonal(z: np.ndarray, m: int = 3) -> np.ndarray:
     return z * ((1 + (m - 2)*z + z**2)) / (1 - z)**3
 
