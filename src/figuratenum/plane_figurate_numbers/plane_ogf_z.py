81932787cb11147bd448a1153f2abb91dcba2a7f
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-05 12:23:09 -0500

    chore: rename file from plane_generating_fz.py to plane_ogf_z.py

---
+++ src/figuratenum/plane_figurate_numbers/plane_ogf_z.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+
+def m_gonal(z: np.ndarray, m: int) -> np.ndarray:
+    return z * ((m - 3)*z + 1) / (1 - z)**3
+
+
+def triangular(z: np.ndarray) -> np.ndarray:
+    return z / (1 - z)**3
+
+
+def square(z: np.ndarray) -> np.ndarray:
+    return z * (z + 1) / (1 - z)**3
+
+
+def pentagonal(z: np.ndarray) -> np.ndarray:
+    return z * (2*z + 1) / (1 - z)**3
+
+
+def hexagonal(z: np.ndarray) -> np.ndarray:
+    return z * (3*z + 1) / (1 - z)**3
+
+
+def centered_m_gonal(z: np.ndarray, m: int) -> np.ndarray:
+    return z * ((1 + (m - 2)*z + z**2)) / (1 - z)**3
+
+
+def pronic(z: np.ndarray) -> np.ndarray:
+    return 2 * z / (1 - z)**3
+
+
+def gnomonic(z: np.ndarray) -> np.ndarray:
+    return z * (1 + z) / (1 - z)**2
+
+
+def truncated_triangular(z: np.ndarray) -> np.ndarray:
+    return z * (1 + z**2 + 4*z) / (1 - z)**3
+
+
+def truncated_square(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 9*z + 4*z**2) / (1 - z)**3
+
+
+def truncated_pronic(z: np.ndarray) -> np.ndarray:
+    return z * (2 + 10*z + 2*z**2) / (1 - z)**3
+
+
+def truncated_centered_m_gonal(z: np.ndarray, m: int) -> np.ndarray:
+    return z * (1 + (5*m - 2)*z + (1 + 2*m)*z**2) / (1 - z)**3
+
+
+def truncated_centered_triangular(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 13*z + 7*z**2) / (1 - z)**3
+
+
+def truncated_centered_square(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 18*z + 9*z**2) / (1 - z)**3
+
+
+def truncated_centered_pentagonal(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 23*z + 11*z**2) / (1 - z)**3
+
+
+def truncated_centered_hexagonal(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 28*z + 13*z**2) / (1 - z)**3
+
+
+def polygram(z: np.ndarray, m: int) -> np.ndarray:
+    return z * (1 + (2*m - 2)*z + z**2) / (1 - z)**3
+
+
+def aztec_diamond(z: np.ndarray) -> np.ndarray:
+    return 4 * z / (1 - z)**3
+
+
+def cross(z: np.ndarray,) -> np.ndarray:
+    return z * (1 + 3*z) / (1 - z)**2
+
+
+def diamond(z: np.ndarray) -> np.ndarray:
+    return z * (1 + 2*z + z**2) / (1 - z)**3
+
+
+def square_triangular(z: np.ndarray) -> np.ndarray:
+    return z * (1 + z) / ((1 - z)*(1 - 34*z + z**2))
+
+# Generalized Plane FigurateNum, n = 0, +/-1, +/-2, ...
+
+
+def generalized_m_gonal(z: np.ndarray, m: int) -> np.ndarray:
+    return z**2 * (1 + z) * (1 + (m - 4)*z + z**2) / (1 - z**2)**3
+
+
+def generalized_centered_m_gonal(z: np.ndarray, m: int) -> np.ndarray:
+    return z**2 * (1 + (m + 1)*z + (m - 2)*z**2 - 2*z**3 + z**4 + z**5) / (1 - z**2)**3
+
+
+def generalized_pronic(z: np.ndarray) -> np.ndarray:
+    return z * (2 + 2*z**3) / (1 - z**2)**3
