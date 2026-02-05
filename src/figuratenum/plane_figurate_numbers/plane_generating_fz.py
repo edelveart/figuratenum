b6cf08e3863c9e9d12a797d70e3169d1df084110
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-04 22:25:56 -0500

    feat(plane-generating-fz): add centered-m-gonal generating function f(z)

--- src/figuratenum/plane_figurate_numbers/plane_generating_fz.py
+++ src/figuratenum/plane_figurate_numbers/plane_generating_fz.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def m_gonal(z: np.ndarray, m: int = 3) -> np.ndarray:
     return z * ((m - 3)*z + 1) / (1 - z)**3
+
+
+def centered_m_gonal(z: np.ndarray, m: int = 3) -> np.ndarray:
+    return z * ((1 + (m - 2) * z + z**2)) / (1 - z)**3
