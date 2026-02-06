36fcca4a650cd89421e1ce4c44f9a977eb2f3027
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-05 20:44:42 -0500

    feat(multidim-ogf-z): add hyperoctahedral generating function f(z)

--- src/figuratenum/multidimensional_figurate_numbers/multidim_ogf_z.py
+++ src/figuratenum/multidimensional_figurate_numbers/multidim_ogf_z.py
@@ -7,3 +7,7 @@ def hypertetrahedral(z: np.ndarray) -> np.ndarray:
 
 def hypercube(z: np.ndarray) -> np.ndarray:
     return z * (1 + 11*z + 11*z**2 + z**3) / (1 - z)**5
+
+
+def hyperoctahedral(z: np.ndarray) -> np.ndarray:
+    return z * (1 + z)**3 / (1 - z)**5
