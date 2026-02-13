ae1434310a60595c4c54cd539b2128784151a792
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-12 19:02:32 -0500

    feat(db-figuratenum): add FigurateNumSchema class with Literal type

---
+++ src/figuratenum/fignum_db/PlaneSchema.py
@@ -0,0 +1,66 @@
+from typing import Literal
+import sympy as sp
+import numpy as np
+
+
+x, m = sp.symbols("x m")
+
+
+PlaneTypes = Literal["m_gonal",
+                     "triangular",
+                     "square",
+                     "pentagonal",
+                     "hexagonal",
+                     "centered_m_gonal",
+                     "pronic",
+                     "gnomonic",
+                     "truncated_triangular",
+                     "truncated_square",
+                     "truncated_pronic",
+                     "truncated_centered_m_gonal",
+                     "truncated_centered_triangular",
+                     "truncated_centered_square",
+                     "truncated_centered_pentagonal",
+                     "truncated_centered_hexagonal",
+                     "polygram",
+                     "aztec_diamond",
+                     "cross",
+                     "diamond",
+                     "square_triangular",
+                     "generalized_m_gonal",
+                     "generalized_centered_m_gonal",
+                     "generalized_pronic"]
+
+
+class PlaneSchema:
+    def __init__(
+        self,
+        name: PlaneTypes,
+        generating_function: sp.Expr,
+        galois_group: str | None = None,
+        galois_description: str | None = None,
+    ):
+        self.name = name
+        self.expression = generating_function
+        self.galois_group = galois_group
+        self.galois_description = galois_description
+        self._lambdified = None
+
+    def lambdify(self):
+        """Generates (once) the numerical version"""
+        if self._lambdified is None:
+            self._lambdified = sp.lambdify((x, m), self.expression, "numpy")
+        return self._lambdified
+
+    def evaluate(self, z: np.ndarray, m_value: int):
+        """Evaluates the generating function f(z)"""
+        return self.lambdify()(z, m_value)
+
+    def analytic_algebraic_properties(self):
+        """Returns summarized metadata."""
+        return {
+            "name": self.name,
+            "generating_function": self.expression,
+            "galois_group": self.galois_group,
+            "group_description": self.galois_description,
+        }
