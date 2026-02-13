6428c5db8dd0908dc9223530a3df4b7d8b06b32c
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-12 23:29:04 -0500

    feat(db-figuratenum): add Space Schema and complete registry of space figurate numbers

---
+++ src/figuratenum/db_figuratenum/SpaceSchema.py
@@ -0,0 +1,94 @@
+from typing import Literal
+import sympy as sp
+import numpy as np
+
+
+x, m = sp.symbols("x m")
+
+SpaceTypes = Literal[
+
+    # Pyramidal
+    "m_pyramidal",
+    "tetrahedral",
+    "cubic",
+    "octahedral",
+    "hauy_octahedral",
+    "icosahedral",
+    "dodecahedral",
+
+    # Archimedean
+    "truncated_tetrahedral",
+    "truncated_cubic",
+    "truncated_octahedral",
+
+    # Star Polyhedral
+    "stella_octangula",
+
+    # Centered
+    "centered_cube",
+    "rhombic_dodecahedral",
+    "hauy_rhombic_dodecahedral",
+    "centered_tetrahedron",
+    "centered_square_pyramid",
+    "centered_pentagonal_pyramid",
+    "centered_hexagonal_pyramid",
+    "centered__mgonal_pyramid",
+    "centered_octahedron",
+    "centered_icosahedron",
+    "centered_dodecahedron",
+
+    # Centered Truncated
+    "centered_truncated_tetrahedron",
+    "centered_truncated_cube",
+    "centered_truncated_octahedron",
+
+    # Centered Pyramidal / Prism
+    "centered_mgonal_pyramidal",
+    "m_prism",
+
+    # Generalized
+    "generalized_mgonal_pyramidal",
+    "generalized_cubic",
+    "generalized_octahedral",
+    "generalized_icosahedral",
+    "generalized_dodecahedral",
+    "generalized_centered_cube",
+    "generalized_centered_tetrahedron",
+    "generalized_centered_square_pyramid",
+    "generalized_rhombic_dodecahedral",
+    "generalized_centered_mgonal_pyramidal",
+    "generalized_hexagonal_prism",]
+
+
+class SpaceSchema:
+    def __init__(
+        self,
+        name: SpaceTypes,
+        generating_function: sp.Expr,
+        galois_group: str | None = None,
+        galois_description: str | None = None,
+    ):
+        self.name = name
+        self.expression = generating_function
+        self.galois_group = galois_group
+        self.galois_description = galois_description
+        self._lambdified = None
+
+    def lambdify(self):
+        """Generates (once) the numerical version"""
+        if self._lambdified is None:
+            self._lambdified = sp.lambdify((x, m), self.expression, "numpy")
+        return self._lambdified
+
+    def evaluate(self, z: np.ndarray, m_sides: int):
+        """Evaluates the generating function f(z)"""
+        return self.lambdify()(z, m_sides)
+
+    def analytic_algebraic_properties(self):
+        """Returns summarized metadata."""
+        return {
+            "name": self.name,
+            "generating_function": self.expression,
+            "galois_group": self.galois_group,
+            "group_description": self.galois_description,
+        }
---
+++ src/figuratenum/db_figuratenum/space_db.py
@@ -0,0 +1,180 @@
+from .SpaceSchema import SpaceSchema, x, m
+
+space_registers = [
+
+    # Pyramidal
+    SpaceSchema(
+        "m_pyramidal",
+        generating_function=x * (1 + (m - 3)*x) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "tetrahedral",
+        generating_function=x / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "cubic",
+        generating_function=x * (1 + 4*x + x**2) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "octahedral",
+        generating_function=x * (x + 1)**2 / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "hauy_octahedral",
+        generating_function=x * (1 + x)**3 / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "icosahedral",
+        generating_function=x * (1 + 8*x + 6*x**2) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "dodecahedral",
+        generating_function=x * (1 + 16*x + 10*x**2) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+
+    # Archimedean
+    SpaceSchema(
+        "truncated_tetrahedral",
+        generating_function=x * (1 + 12*x + 10*x**2) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "truncated_cubic",
+        generating_function=x * (1 + 52*x + 93*x**2 + 8*x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "truncated_octahedral",
+        generating_function=x * (1 + 34*x + 55*x**2 + 6*x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+
+    # Star
+    SpaceSchema(
+        "stella_octangula",
+        generating_function=x * (1 + 10*x + x**2) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+
+    # Centered
+    SpaceSchema(
+        "centered_cube",
+        generating_function=x * (1 + 5*x + 5*x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "rhombic_dodecahedral",
+        generating_function=x * (1 + 11*x + 11*x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "hauy_rhombic_dodecahedral",
+        generating_function=x * (1 + 29*x + 59*x**2 + 7*x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+
+    # Centered Pyramids
+    SpaceSchema(
+        "centered_tetrahedron",
+        generating_function=x * (1 + x + x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "centered_square_pyramid",
+        generating_function=x * (1 + 2*x + 2*x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "centered_pentagonal_pyramid",
+        generating_function=x * (1 + 3*x + 3*x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "centered_hexagonal_pyramid",
+        generating_function=x * (1 + 4*x + 4*x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "centered__mgonal_pyramid",
+        generating_function=x *
+        (1 + (m - 2)*x + (m - 2)*x**2 + x**3) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+
+    # Prism
+    SpaceSchema(
+        "m_prism",
+        generating_function=x * (1 + (2*m - 2)*x + (m + 1)*x**2) / (1 - x)**4,
+        galois_group="",
+        galois_description=""
+    ),
+]
+
+
+generalized_space_registers = [
+
+    SpaceSchema(
+        "generalized_mgonal_pyramidal",
+        generating_function=x**2 *
+        (1 + (m - 3)*x**2 - (m - 3)*x**3 - x**5) / (1 - x**2)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "generalized_cubic",
+        generating_function=x**2 *
+        (1 - x) * (1 + 4*x**2 + x**4) / (1 - x**2)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "generalized_octahedral",
+        generating_function=x**2 * (1 - x) * (1 + x**2)**2 / (1 - x**2)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "generalized_icosahedral",
+        generating_function=x**2 *
+        (1 - 6*x + 8*x**2 - 8*x**3 + 6*x**4 - x**5) / (1 - x**2)**4,
+        galois_group="",
+        galois_description=""
+    ),
+    SpaceSchema(
+        "generalized_dodecahedral",
+        generating_function=x**2 *
+        (1 - 10*x + 16*x**2 - 16*x**3 + 10*x**4 - x**5) / (1 - x**2)**4,
+        galois_group="",
+        galois_description=""
+    ),
+]
+
+space_figuratenum_registers = space_registers + generalized_space_registers
+
+SPACE_DATABASE = {seq.name: seq for seq in space_figuratenum_registers}
