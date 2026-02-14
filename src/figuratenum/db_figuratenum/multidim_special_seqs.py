18a0e39504afc1484017c72f1e1759060aaafe09
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-13 22:36:25 -0500

    feat(db-figuratenum/multidim_db): add k_dim_hypercube_x generating function

---
+++ src/figuratenum/db_figuratenum/multidim_special_seqs.py
@@ -0,0 +1,7 @@
+from .symbols_figuratenum import sp, x, k
+from sympy.abc import i, j
+
+
+a_i_k = sp.Sum((-1)**j * sp.binomial(k + 1, j) * (i + 1 - j)**k, (j, 0, i))
+
+k_dim_hypercube_x = x * sp.Sum(a_i_k * x**i, (i, 0, k - 1)) / (1 - x)**(k + 1)
