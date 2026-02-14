dbf07aba6601353098733bc4511fe3a2feb2e7f1
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-13 22:44:17 -0500

    feat(db-figuratenum/multidim_db): add generalized_k_dim_hypercube_x generating function

--- src/figuratenum/db_figuratenum/multidim_special_seqs.py
+++ src/figuratenum/db_figuratenum/multidim_special_seqs.py
@@ -15,3 +15,11 @@ k_dim_nexus_x = x * sp.Sum(a_i_k_plus_1 * x**i, (i, 0, k)) / (1 - x)**(k + 1)
 
 # K-Dimensional Centered
 k_dim_centered_hypercube_x = (1 + x) * k_dim_hypercube_x
+
+
+# Generalized
+pg_hypercube = x**2 * (1 + (-1)**k * x)
+g_num_hypercube = sp.Sum(a_i_k * x**(2 * i), (i, 0, k - 1))
+g_den_hypercube = (1 - x**2)**(k + 1)
+
+generalized_k_dim_hypercube_x = pg_hypercube * g_num_hypercube / g_den_hypercube
