e0ebe23e30f39a09920beefee9c94c26cbb8175a
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-13 22:38:12 -0500

    feat(db-figuratenum/multidim_db): add k_dim_nexus_x generating function

--- src/figuratenum/db_figuratenum/multidim_special_seqs.py
+++ src/figuratenum/db_figuratenum/multidim_special_seqs.py
@@ -5,3 +5,9 @@ from sympy.abc import i, j
 a_i_k = sp.Sum((-1)**j * sp.binomial(k + 1, j) * (i + 1 - j)**k, (j, 0, i))
 
 k_dim_hypercube_x = x * sp.Sum(a_i_k * x**i, (i, 0, k - 1)) / (1 - x)**(k + 1)
+
+
+a_i_k_plus_1 = sp.Sum((-1)**j * sp.binomial(k + 2, j)
+                      * (i + 1 - j)**(k + 1), (j, 0, i))
+
+k_dim_nexus_x = x * sp.Sum(a_i_k_plus_1 * x**i, (i, 0, k)) / (1 - x)**(k + 1)
