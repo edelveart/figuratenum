ad2f3ab5906bf33072835accfa142801fad306d7
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-14 23:29:55 -0500

    test(plane-series): add test series with both symbolic and numeric methods

--- tests/test_plane_series.py
+++ tests/test_plane_series.py
@@ -1,3 +1,4 @@
+import pytest
 from src.figuratenum.plane_figurate_numbers.PlaneFigurateNum import PlaneFigurateNum
 from src.figuratenum.NumCollector import NumCollector as nc
 from src.figuratenum.figurate_viz.PlaneCViz import PlaneCViz
@@ -8,157 +9,177 @@ pfgn = PlaneFigurateNum()
 n = 20
 
 
-def test_triangular_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_triangular_series(dual_method):
     expected = nc.take_to_list(pfgn.triangular(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'triangular', n_terms=n+1, coeffs=True, method='numeric')
+        'triangular', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_square_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_square_series(dual_method):
     expected = nc.take_to_list(pfgn.square(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'square', n_terms=n+1, coeffs=True, method='numeric')
+        'square', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_pentagonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_pentagonal_series(dual_method):
     expected = nc.take_to_list(pfgn.pentagonal(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'pentagonal', n_terms=n+1, coeffs=True, method='numeric')
+        'pentagonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_hexagonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_hexagonal_series(dual_method):
     expected = nc.take_to_list(pfgn.hexagonal(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'hexagonal', n_terms=n+1, coeffs=True, method='numeric')
+        'hexagonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_centered_mgonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_centered_mgonal_series(dual_method):
     expected = nc.take_to_list(pfgn.centered_mgonal(9), n)
     plane = PlaneCViz(m=9)
     result = plane.expand_series(
-        'centered_mgonal', n_terms=n+1, coeffs=True, method='numeric')
+        'centered_mgonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_polygonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_polygonal_series(dual_method):
     expected = nc.take_to_list(pfgn.polygonal(17), n)
     plane = PlaneCViz(m=17)
     result = plane.expand_series(
-        'polygonal', n_terms=n+1, coeffs=True, method='numeric')
+        'polygonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_pronic_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_pronic_series(dual_method):
     expected = nc.take_to_list(pfgn.pronic(), n)
     plane = PlaneCViz(m=17)
     result = plane.expand_series(
-        'pronic', n_terms=n+1, coeffs=True, method='numeric')
+        'pronic', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_gnomonic_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_gnomonic_series(dual_method):
     expected = nc.take_to_list(pfgn.gnomonic(), n)
     plane = PlaneCViz(m=17)
     result = plane.expand_series(
-        'gnomonic', n_terms=n+1, coeffs=True, method='numeric')
+        'gnomonic', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_triangular_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_triangular_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_triangular(), n)
     plane = PlaneCViz(m=17)
     result = plane.expand_series(
-        'truncated_triangular', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_triangular', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_square_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_square_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_square(), n)
     plane = PlaneCViz(m=17)
     result = plane.expand_series(
-        'truncated_square', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_square', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_pronic_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_pronic_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_pronic(), n)
     plane = PlaneCViz(m=17)
     result = plane.expand_series(
-        'truncated_pronic', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_pronic', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_centered_mgonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_centered_mgonal_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_centered_mgonal(11), n)
     plane = PlaneCViz(m=11)
     result = plane.expand_series(
-        'truncated_centered_mgonal', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_centered_mgonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_centered_square_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_centered_square_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_centered_square(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'truncated_centered_square', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_centered_square', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_centered_pentagonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_centered_pentagonal_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_centered_pentagonal(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'truncated_centered_pentagonal', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_centered_pentagonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_truncated_centered_hexagonal_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_truncated_centered_hexagonal_series(dual_method):
     expected = nc.take_to_list(pfgn.truncated_centered_hexagonal(), n)
     plane = PlaneCViz(m=3)
     result = plane.expand_series(
-        'truncated_centered_hexagonal', n_terms=n+1, coeffs=True, method='numeric')
+        'truncated_centered_hexagonal', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_polygram_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_polygram_series(dual_method):
     expected = nc.take_to_list(pfgn.polygram(12), n)
     plane = PlaneCViz(m=12)
     result = plane.expand_series(
-        'polygram', n_terms=n+1, coeffs=True, method='numeric')
+        'polygram', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_aztec_diamond_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_aztec_diamond_series(dual_method):
     expected = nc.take_to_list(pfgn.aztec_diamond(), n)
     plane = PlaneCViz(m=12)
     result = plane.expand_series(
-        'aztec_diamond', n_terms=n+1, coeffs=True, method='numeric')
+        'aztec_diamond', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_cross_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_cross_series(dual_method):
     expected = nc.take_to_list(pfgn.cross(), n)
     plane = PlaneCViz(m=12)
     result = plane.expand_series(
-        'cross', n_terms=n+1, coeffs=True, method='numeric')
+        'cross', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_diamond_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_diamond_series(dual_method):
     expected = nc.take_to_list(pfgn.diamond(), n)
     plane = PlaneCViz(m=12)
     result = plane.expand_series(
-        'diamond', n_terms=n+1, coeffs=True, method='numeric')
+        'diamond', n_terms=n+1, coeffs=True, method=dual_method)
     assert result == expected
 
 
-def test_square_triangular_series():
+@pytest.mark.parametrize("dual_method", ["symbolic", "numeric"])
+def test_square_triangular_series(dual_method):
     pass
