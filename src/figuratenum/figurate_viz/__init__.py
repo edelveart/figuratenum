df51b172e1df64547bd39e2050e19e27eaba940f
Edgar Delgado Vega <edelve91@gmail.com>
2026-02-18 13:33:48 -0500

    chore(figurateviz): expose PowerSeriesExpansion in __init__.py

--- src/figuratenum/figurate_viz/__init__.py
+++ src/figuratenum/figurate_viz/__init__.py
@@ -1,5 +1,6 @@
 from .DiscreteViz import DiscreteViz
 from .ComplexViz import ComplexViz
+from .SeriesExpansion import PowerSeriesExpansion
 
 # Deprecated: Temporal alias for transition. Will be removed in version 4.0.0
 FigurateViz = DiscreteViz
